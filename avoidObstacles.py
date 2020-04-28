5318ada682dcab8b5e963457d573a854e11bd943
hieulv <lehieubkhn117@gmail.com>
2020-04-28 09:06:44 +0700

    add avoidObstacles

---
+++ avoidObstacles.py
@@ -0,0 +1,11 @@
+def avoidObstacles(inputArray):
+    inputArray.sort()
+    last_value = inputArray[len(inputArray) - 1]
+    for i in range(2, last_value + 2):
+        if all(n % i != 0 for n in inputArray):
+            return i
+        i += 1
+                   
+
+inputArray = [1, 4, 10, 6, 2]
+print(avoidObstacles(inputArray))
