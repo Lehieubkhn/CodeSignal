e5a6194956147036bb2ae01719897424eb599036
hieulv <lehieubkhn117@gmail.com>
2020-05-02 11:09:39 +0700

    add bishopAndPawn

---
+++ bishopAndPawn.py
@@ -0,0 +1,25 @@
+def bishopAndPawn(bishop, pawn):
+    if isSameColor(bishop) != isSameColor(pawn):
+        return False
+    if isSameRow(bishop) == isSameRow(pawn):
+        return False
+    if isSameColumn(bishop) == isSameColumn(pawn):
+        return False
+    return True
+
+
+def isSameColor(arr):
+    color = ord(arr[0]) + ord(arr[1])
+    if color % 2 == 0:
+        return True
+    return False
+
+def isSameRow(arr):
+    return ord(arr[1])
+
+def isSameColumn(arr):
+    return ord(arr[0])
+
+bishop = "c1"
+pawn = "c3"
+print(bishopAndPawn(bishop, pawn))
